790e0fca94008f5c4c058e5eedad347c066623f9
Selene Banuelos <selenebanuelos@berkeley.edu>
2025-11-26 16:03:34 -0800

    Identified samples ran more than once

--- processed-reruns.R
+++ processed-reruns.R
@@ -5,6 +5,7 @@
 
 # setup 
 library(dplyr)
+library(stringr)
 
 # save raw data file names as list
 file_names <- list.files(path = 'data-raw/t5/',
@@ -17,10 +18,35 @@ raw_data <- purrr::map_dfr(file_names,
                    function(x) read.csv(x) %>% mutate(file_name = x)
                    )
 
-# identify reruns
-reruns <- raw_data %>%
+# clean up raw data
+clean_data <- raw_data %>%
   # create run_date column
-  mutate(run_date = stringr::str_extract(
+  mutate(batch = str_extract(
     file_name, 
-    'TEL_(.*?) - Quant')
+    '(?<=TEL_)(.*)(?= - Quant)') # (?<=prefix)(keep)(?=suffix)
+    ) %>%
+  select(c(Sample, Content, batch, contains('Cq'))) %>%
+  # remove any rows corresponding to controls: Content = 'Std', 'Ctrl', 'NTC'
+  filter(
+    str_detect(Content, # column to filter on
+                        "Std|Ctrl|NTC", 
+                        negate = TRUE # keep rows that don't have these strings
+                        )
+  ) %>%
+  # strip any leading zeros from sample names
+  mutate(Sample = str_remove(
+    Sample,
+    '^0+') # match any number of zeros at the beginning of a string
     )
+
+# identify reruns
+reruns <- clean_data %>%
+  group_by(Sample) %>%
+  filter(n_distinct(batch) >1) %>% # keep samples that were run in > 1 batch
+  distinct(Sample, batch)
+
+# all samples run
+all_ids <- unique(clean_data$Sample)
+
+# all samples run more than once
+rerun_ids <- unique(reruns$Sample)
