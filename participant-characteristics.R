6b4fbe44159ee585f00df9f123b98506e46f45fa
selenebanuelos <135679586+selenebanuelos@users.noreply.github.com>
2025-12-11 14:49:38 -0800

    Update participant-characteristics.R

--- participant-characteristics.R
+++ participant-characteristics.R
@@ -18,35 +18,103 @@ sex <- read.csv('data-raw/pearlsbio_data_SeleneBanuelos_2025_08_06.csv') %>%
 # import buccal DNA QC data
 dna_qc <- read.csv('data-processed/dna-qc.csv')
 
+# explore t2
+t2_no_sample <- dna_qc %>%
+  filter(visitnum == 2,
+         is.na(specimenid))
+
+t2_low_dna <- dna_qc %>%
+  filter(visitnum ==2,
+         !is.na(specimenid)
+  ) %>%
+  filter(dna_conc_5 == 0)
+
+t2_contaminated <- dna_qc %>%
+  filter(visitnum ==2,
+         !is.na(specimenid)
+  ) %>%
+  filter(dna_pure == 0)
+
+t2_passed <- dna_qc %>%
+  filter(visitnum == 2, 
+         dna_qc_passed ==1)
+  
+# explore t5
+t5_no_sample <- dna_qc %>%
+  filter(visitnum == 5,
+         is.na(specimenid))
+
+t5_low_dna <- dna_qc %>%
+  filter(visitnum ==5,
+         !is.na(specimenid)
+  ) %>%
+  filter(dna_conc_5 == 0)
+
+t5_contaminated <- dna_qc %>%
+  filter(visitnum ==5,
+         !is.na(specimenid)
+  ) %>%
+  filter(dna_pure == 0)
+
+t5_passed <- dna_qc %>%
+  filter(visitnum == 5, 
+         dna_qc_passed ==1)
+
+# for t2 samples that passed DNA QC, average concentration and 260/280 measures
+t2 <- dna_qc %>%
+  filter(visitnum ==2) %>%
+  mutate(dna_conc = signif( ( (conc_DZ+conc_RD) / 2), digits = 4 ),
+         dna_purity = signif( ( (purity_DZ+purity_RD) / 2), digits = 3)
+  ) %>%
+  select(c(pearls_id, specimenid, visitnum, dna_conc, dna_purity, dna_qc_passed))
+
+# clean up t5 data for merge with t2
+t5 <- dna_qc %>%
+  filter(visitnum == 5) %>%
+  select(c(pearls_id, specimenid, visitnum, os_dna_conc, purity, dna_qc_passed)) %>%
+  rename('dna_conc' = 'os_dna_conc',
+         'dna_purity' = 'purity')
+
+t2_t5_passed <- rbind(filter(t2, dna_qc_passed == 1),
+                      filter(t5, dna_qc_passed == 1)
+                      )
 
 # merge demographics data with buccal DNA info
 merged <- pearls %>%
-  filter(visitnum == 1 | visitnum == 2) %>%
-  select(c(pearls_id, 
+  filter(visitnum == 2) %>%
+  select(c(pearls_id,
            visitnum,
-           child_age_years_demo,
-           child_age_years_exam,
-           today_s_date_r
+           child_age_years_exam
            )
          ) %>%
   full_join(.,
-            dna_qc,
+            t2_t5_passed,
             by = c('pearls_id', 'visitnum')
   ) %>%
-  full_join(., sex, by = 'pearls_id')
+  full_join(., sex, by = 'pearls_id') %>%
+  mutate(missing_sample = case_when(is.na(specimenid) ~ 1,
+                                    .default = 0)
+         )
 
-# create summary table
+# create summary table of those with buccal DNA samples
 merged %>%
-  filter(visitnum == 2 | visitnum == 5) %>%
   tbl_summary(by = visitnum, # stratify table by timepoint
-              include = c(dna_qc_passed,
-                          child_age_years_exam,
-                          sex)
+              include = c(child_age_years_exam,
+                          sex),
+              missing_text = 'Missing'
               )
 
-# create reasons-for-missing table
+# create summary table of those missing buccal DNA samples
 merged %>%
-  filter(dna_qc_passed == 0) %>%
+  filter(visitnum == 2 | visitnum == 5,
+         dna_qc_passed == 0 | is.na(dna_qc_passed)
+         ) %>%
   tbl_summary(by = visitnum,
-               include = c(dna_conc_5,
-                           dna_pure))
+              include = c(dna_conc_5,
+                          dna_pure,
+                          missing_sample),
+              statistic = list(
+                all_categorical() ~ '{n} ({p}%)'
+              ),
+              missing_text = 'Missing'
+  )
